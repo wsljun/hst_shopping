e7395c190a2010023efe54f7fecdd8c462fc147c
wsljun <wsljun02@163.com>
2018-09-19 18:02:01 +0800

    Merge branch 'master' of https://github.com/wsljun/hst_shopping
    # Conflicts:
    #	app/src/main/java/com/cj/reocrd/api/UrlConstants.java
    #	uikit/build/intermediates/blame/res/androidTest/debug/single/debug.json
    #	uikit/build/intermediates/incremental/mergeDebugAndroidTestResources/compile-file-map.properties
    #	uikit/build/intermediates/incremental/packageDebugResources/compile-file-map.properties

--- app/src/main/java/com/cj/reocrd/api/UrlConstants.java
+++ app/src/main/java/com/cj/reocrd/api/UrlConstants.java
@@ -16,7 +16,7 @@ public class UrlConstants {
     public final static String URL = "app/outinterface";
     public final static String URL_ALIPAY_RESULT = BASE_URL+"api/alipayResult";
     public final static String URL_ALIPAY_RECHARGE = BASE_URL+"api/alipayRecharge";
-    public final static String SUCCESE_CODE = "1"; // 返回结果成功
+    public final static String SUCCESE_CODE = "1"; // 返回结果成
     public final static String URL_SHARE = BASE_URL+"api/shareMerch?id=";
     public final static String URL_AGREE = BASE_URL + "api/agreement";
 
